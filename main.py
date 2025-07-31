d77bf58f1de9eef6c24badc21f72df0685254b32
RafaelBorges22 <rafaelmascarenhasborges@gmail.com>
2025-07-31 19:15:10 -0300

    Request CRUD created successfully :sparkles:

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from routers.StockRouters import stock_blueprint
 from routers.AdminRouters import admin_blueprint 
 from routers.EmailRouters import email_blueprint
 from routers.DriverRouters import driver_blueprint
+from routers.SolicitationRouters import solicitation_blueprint
 from dotenv import load_dotenv
 from flask_cors import CORS
 
@@ -29,6 +30,7 @@ app.register_blueprint(stock_blueprint, url_prefix = "/stock")
 app.register_blueprint(admin_blueprint, url_prefix = "/admin")
 app.register_blueprint(email_blueprint, url_prefix = "/email")
 app.register_blueprint(driver_blueprint, url_prefix = "/driver")
+app.register_blueprint(solicitation_blueprint, url_prefix = "/solicitation")
 
 if __name__ == '__main__':
     with app.app_context():
---
+++ models/SolicitationModel.py
@@ -0,0 +1,18 @@
+from database.db import db
+from enums.SolicitationEnum import SolicitationEnum
+
+class SolicitationModel(db.Model):
+    __tablename__ = 'solicitations'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    client_id = db.Column(db.Integer, db.ForeignKey('clients.id'), nullable=False)
+    adress = db.Column(db.String(255), nullable=False)
+    status = db.Column(db.String(20), nullable=False, default=SolicitationEnum.PENDING.value)
+    description = db.Column(db.Text, nullable=True)
+    date_solicitation = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+    date_collected = db.Column(db.DateTime, nullable=True)
+    
+    client = db.relationship('ClientModel', back_populates='solicitations')
+
+    def __repr__(self):
+        return f'<Solicitation(client_id={self.client_id}, status={self.status}, adress={self.adress})>'
