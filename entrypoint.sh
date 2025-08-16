8ccde027fb3fc6d3c3e5193b2c63fea6e648e4a8
RafaelBorges22 <rafaelmascarenhasborges@gmail.com>
2025-08-15 22:47:52 -0300

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ export FLASK_APP=src.main:app
 echo "Waiting for database..."
 sleep 10
 
-until /usr/local/bin/python3 -m flask db upgrade
+until /usr/local/bin/python3 -m flask db upgrade 2>&1
 do
   echo "Database migration failed. Retrying in 5 seconds..."
   sleep 5
