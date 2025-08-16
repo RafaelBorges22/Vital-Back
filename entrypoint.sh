a97fc9b2e8fb53d97b670bb6772628d9da1eac55
RafaelBorges22 <rafaelmascarenhasborges@gmail.com>
2025-08-15 23:22:03 -0300

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
-set -e
+set -x
 
-# Mude para o diretório de trabalho do aplicativo.
-cd /app
-
-# Garanta que o PYTHONPATH esteja definido.
-export PYTHONPATH=./src
-
-echo "Iniciando a migração do banco de dados..."
-
-# Execute a migração e capture a saída para diagnóstico.
-python3 -m flask db upgrade
-
-echo "Migração bem-sucedida. Iniciando Gunicorn..."
-
-# Inicie o Gunicorn. O 'exec' substitui o processo do shell,
-# o que é uma boa prática em contêineres Docker.
-exec gunicorn src.main:app -b 0.0.0.0:$PORT
+echo "DATABASE_URI: $DATABASE_URI"
+echo "Done."
