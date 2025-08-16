702c712cafd3b0dfb13bb2510a9fe2cf77f4ed88
RafaelBorges22 <rafaelmascarenhasborges@gmail.com>
2025-08-15 22:43:47 -0300

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-# Define a variável de ambiente FLASK_APP
 export FLASK_APP=src.main:app
 
-# Executa a migração do banco de dados
-/usr/local/bin/python3 -m flask db upgrade
+echo "Waiting for database..."
+sleep 10
 
-# Executa o comando principal (Gunicorn, passado pelo CMD)
+until /usr/local/bin/python3 -m flask db upgrade
+do
+  echo "Database migration failed. Retrying in 5 seconds..."
+  sleep 5
+done
+
+echo "Migration successful. Starting application..."
 exec "$@"
